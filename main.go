12f2030fdce89ab8fb0130216c743d8906ed27a8
Andres Rios <rianby64@gmail.com>
2019-09-15 00:47:45 +0300

    Comencemos con articles

--- main.go
+++ main.go
@@ -6,16 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+// SetupRouter the server
+func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	// gin.H is a shortcut for map[string]interface{}
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hey",
-			"status":  http.StatusOK,
+			"links": map[string]interface{}{
+				"self": "/",
+			},
 		})
 	})
+
+	r.GET("/api/articles", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"links": map[string]interface{}{
+				"self": "api/articles",
+			},
+			"data": []interface{}{},
+		})
+	})
+
+	return r
+}
+
+func main() {
+	r := SetupRouter()
 
 	// Listen and serve on 0.0.0.0:8080
 	r.Run(":8080")
--- main_test.go
+++ main_test.go
@@ -1,29 +1,76 @@
 package main_test
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+
+	main "github.com/rianby64/gin-example"
 )
 
 var host string
+var port string
+var ErrorNot200OK = errors.New("Status is not 200")
+var mockRouter *gin.Engine
 
 func init() {
 	err := godotenv.Load()
 	if err == nil {
 		host = os.Getenv("HOST")
+		port = os.Getenv("PORT")
 	}
+	fmt.Printf("Running host=%s\n", host)
+	mockRouter = main.SetupRouter()
+}
+
+func requestResponse(from string) (interface{}, error) {
+	resp := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:%s/%s", host, port, from), nil)
+	mockRouter.ServeHTTP(resp, req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Code != 200 {
+		return nil, ErrorNot200OK
+	}
+
+	var response interface{}
+	err = json.Unmarshal([]byte(resp.Body.String()), &response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
 
 func Test_check_connection(t *testing.T) {
-	resp, err := http.Get(fmt.Sprintf("http://%s:8080/", host))
-
+	_, err := requestResponse("")
 	if err != nil {
 		t.Fatal(err)
 	}
+}
 
-	t.Log(resp.Status)
+func Test_articles_CRUD__OK(t *testing.T) {
+	t.Run("1 - read empty", func(t *testing.T) {
+		resp, err := requestResponse("api/articles")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := map[string]interface{}{
+			"links": map[string]interface{}{
+				"self": "api/articles",
+			},
+			"data": []interface{}{},
+		}
+		assert.Equal(t, expected, resp)
+	})
 }
